0f6dd2d5231197c24cad7fb7f8a22cd1c4c89490
Andentu <severalmaz97@gmail.com>
2023-03-03 03:01:09 +0300

    Добавил код рекурсии в методе. Добавил комментарии

--- New_5_5_1/Program.cs
+++ New_5_5_1/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep); // Передаем значение переменных в метод
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string phrase, int deep) //Инициализируем метод, получаем значения переменных
+        {
+            Console.WriteLine(phrase); //Вывод на экран при каждом вызове метода
+
+            if (deep > 1) //Работет до тех пор, пока deep больше 1
+            {
+                Echo(phrase, deep - 1); //Здесь метод вызывает сам себя, пока deep не будет равен 1
+            }
         }
     }
 }
